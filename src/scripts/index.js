e996d7e6843b528887ae02f5d7e4e74561c16e3b
Marcos Vinicius Goulart <mvalvesgoulart@gmail.com>
2023-11-10 14:00:09 -0300

    Quase Finalizado.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,3 +18,61 @@ const colors = [
 colors.forEach((style, index) => {
   inputsColor[index].value = style;
 });
+
+const changeColor = () => {
+  colors.forEach((styles, index) => {
+    inputsColor[index].addEventListener("input", (e) => {
+      colors[index] = e.target.value
+    })
+  });
+  return colors;
+}
+
+const createColorVariable = (info) => {
+  const variables = [
+    `--color-brand-100: ${info[0]};`,
+    `--color-brand-200: ${info[1]};`,
+    `--color-brand-300: ${info[2]};`,
+    `--color-brand-400: ${info[3]};`,
+    `--color-grey-100: ${info[4]};`,
+    `--color-grey-200: ${info[5]};`,
+    `--color-grey-300: ${info[6]};`,
+    `--color-grey-400: ${info[7]};`,
+    `--color-feedback-alert: ${info[8]};`,
+    `--color-feedback-success: ${info[9]};`,
+    `--color-feedback-warning: ${info[10]};`,
+    `--color-feedback-disable: ${info[11]};`,
+  ]
+  return variables;
+}
+
+const createFontVariable = () => {
+  const inputFont = document.querySelector("#font-scale")
+  const arrFont = inputFont.value.split(/\s*,\s*/);
+  const fontFormated = [];
+  arrFont.forEach((value, index) => {
+    fontFormated.push(
+      `--font-size-${index + 1}: ${value}rem;`
+    )
+  })
+  return fontFormated;
+}
+
+const allVariables = (color, font) => {
+  const varInfo = color.concat(font);
+  const formatedVar =  varInfo.join("\n");
+  return formatedVar;
+}
+
+const copyStyles = () =>{
+  const btnCopy = document.querySelector(".form__buttons--copy");
+  btnCopy.addEventListener("click", (e) =>{
+    event.preventDefault();
+    console.log(allVariables(createColorVariable(changeColor()), createFontVariable()))
+    navigator.clipboard.writeText(allVariables(createColorVariable(changeColor()), createFontVariable()))
+  })
+}
+
+changeColor()
+copyStyles();
+
